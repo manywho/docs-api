d09f363ff315463337c7c68e278a58c3ed65c5b7
Jonjo McKay <jonjo.mckay@manywho.com>
2018-06-01 14:34:13 +0100

    Fixed hanging line

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,7 @@ git config --global user.name "Boomi Flow"
 git clone $GIT_ORIGIN .
 
 # Then we generate the static HTML
-redoc-cli bundle https://flow.manywho.com/swagger/v1/swagger.json --output=index.html --title="Boomi Flow API Documentation" --options.nativeScrollbars --options.theme.colors.main=#007db8 --options.noAutoAuth --options.lazyRendering 
---options.requiredPropsFirst --options.suppressWarning
+redoc-cli bundle https://flow.manywho.com/swagger/v1/swagger.json --output=index.html --title="Boomi Flow API Documentation" --options.nativeScrollbars --options.theme.colors.main=#007db8 --options.noAutoAuth --options.lazyRendering --options.requiredPropsFirst --options.suppressWarning
 
 # Now we can commit and push
 git add index.html && git commit -m "Updated documentation"
